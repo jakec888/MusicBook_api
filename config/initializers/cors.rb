50307199504e9e8e18199b54444063bb495a7f05
chef <cgcook81@gmail.com>
2019-03-04 16:29:20 -0500

    updated heroku url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001','https://git.heroku.com/music-book-react.git'
+    origins 'localhost:3001','https://git.heroku.com/stark-cove-90859.git'
 
 
     resource '*',
