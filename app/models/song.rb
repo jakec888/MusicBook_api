e98cb1347b26ca6d1ba85b4b59f337f0455ae115
chef <cgcook81@gmail.com>
2019-03-04 19:21:25 -0500

    change

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,7 +14,7 @@ class Song
 
   if(ENV['DATABASE_URL'])
     uri = URI.parse(ENV['DATABASE_URL'])
-    DB = PG.connect(uri.hostnsme, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   else
     DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'MusicBook_api_development'})
   end
